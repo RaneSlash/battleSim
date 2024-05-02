7902bd0b133bc8689169332c706d3bed09edb823
Rane <ethanajones117@gmail.com>
2024-05-02 13:46:04 -0500

    Add files via upload

--- battleSim.js
+++ battleSim.js
@@ -10,10 +10,45 @@ const ARMOR = [
     {name: "light", prot: 30, res: 10}
 ];
 
-function meleeStats () {
+function pmeleeStats () {
     document.getElementById("classP").innerHTML = "Class: Melee";
     document.getElementById("weapP").innerHTML = "Weapon: Iron Sword";
     document.getElementById("armorP").innerHTML = "Armor: Heavy";
 }
 
-document.getElementById("melee").addEventListener("click", meleeStats);
+function prangerStats () {
+    document.getElementById("classP").innerHTML = "Class: Ranger";
+    document.getElementById("weapP").innerHTML = "Weapon: Comp Bow";
+    document.getElementById("armorP").innerHTML = "Armor: Medium";
+}
+
+function pmageStats () {
+    document.getElementById("classP").innerHTML = "Class: Mage";
+    document.getElementById("weapP").innerHTML = "Weapon: Mage Staff";
+    document.getElementById("armorP").innerHTML = "Armor: Light";
+}
+
+function emeleeStats () {
+    document.getElementById("classE").innerHTML = "Class: Melee";
+    document.getElementById("weapE").innerHTML = "Weapon: Iron Sword";
+    document.getElementById("armorE").innerHTML = "Armor: Heavy";
+}
+
+function erangerStats () {
+    document.getElementById("classE").innerHTML = "Class: Ranger";
+    document.getElementById("weapE").innerHTML = "Weapon: Comp Bow";
+    document.getElementById("armorE").innerHTML = "Armor: Medium";
+}
+
+function emageStats () {
+    document.getElementById("classE").innerHTML = "Class: Mage";
+    document.getElementById("weapE").innerHTML = "Weapon: Mage Staff";
+    document.getElementById("armorE").innerHTML = "Armor: Light";
+}
+
+document.getElementById("pmelee").addEventListener("click", pmeleeStats);
+document.getElementById("pranger").addEventListener("click", prangerStats);
+document.getElementById("pmage").addEventListener("click", pmageStats);
+document.getElementById("emelee").addEventListener("click", emeleeStats);
+document.getElementById("eranger").addEventListener("click", erangerStats);
+document.getElementById("emage").addEventListener("click", emageStats);
